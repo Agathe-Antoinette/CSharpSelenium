fb10cd086dfd33d131473f6c026bd723ddedd8b5
Antoaneta <petkova.antoaneta@gmail.com>
2017-07-26 20:32:51 +0300

    WIP

---
+++ CSharp/Exercise1/CSharpTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise1.Types;
+
+namespace Exercise1
+{
+    class CSharpTypes
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int numberTwo;
+            int numberTree = default(int);
+            var numberFour = default(int);
+            decimal money = 100.5M;
+            var moneyTwo = 100.5M;
+
+            string text = "Hello";
+            Money myMoney = new Money(money);
+            String textTwo = "Hello";
+            var moneyTwoType = moneyTwo.GetType();
+            bool isEqual = 10 == 10;
+
+            //Console.WriteLine(myMoney.Value);
+
+            //if (isEqual)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            decimal result = CalculateArea(money, moneyTwo);
+
+            Console.WriteLine(result);
+        }
+        private static decimal CalculateArea(decimal a, decimal b)
+        {
+            decimal result = a * b;
+
+            return result;
+        }
+    }
+}
---
+++ CSharp/Exercise1/Types/Money.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1.Types
+{
+    class Money
+    {
+        private decimal value;
+        
+        public Money(decimal value)
+        {
+            this.value = value;
+        }
+
+        public decimal Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public decimal GetValue()
+        {
+            return this.value;
+        }
+
+        public void SetValue(decimal value)
+        {
+            this.value = value;
+        }
+    }
+}
